abd3ddd65abcd8565b291e14287e3e91a90ea529
NguyenNhatMinh <dienhoanguyen11@gmail.com>
2025-05-27 15:36:13 +0700

    fix

--- src/member/dtos/create-member.dto.ts
+++ src/member/dtos/create-member.dto.ts
@@ -2,14 +2,14 @@ import { IsNotEmpty, IsNumber, IsUUID } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateMemberDto {
-  @ApiProperty({ example: 100, description: 'Điểm thành viên' })
+  @ApiProperty({ example: 100, description: 'Member Point' })
   @IsNotEmpty()
   @IsNumber()
   SCORE: number;
 
   @ApiProperty({
     example: '93cb78e4-7d41-433e-b4bc-6a0e15d839a6',
-    description: 'ID tài khoản liên kết',
+    description: 'ID Member Account',
   })
   @IsNotEmpty()
   @IsUUID()
--- src/member/dtos/update-member.dto.ts
+++ src/member/dtos/update-member.dto.ts
@@ -2,14 +2,14 @@ import { IsNumber, IsOptional, IsUUID } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateMemberDto {
-  @ApiPropertyOptional({ example: 200, description: 'Điểm thành viên' })
+  @ApiPropertyOptional({ example: 200, description: 'Member Point' })
   @IsOptional()
   @IsNumber()
   SCORE?: number;
 
   @ApiPropertyOptional({
     example: '93cb78e4-7d41-433e-b4bc-6a0e15d839a6',
-    description: 'ID tài khoản liên kết',
+    description: 'ID Member Account',
   })
   @IsOptional()
   @IsUUID()
