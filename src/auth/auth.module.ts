9df6d270f4a0598bfe5e029fdaeb9f7390d25ad2
Shu1237 <ttei8191@gmail.com>
2025-05-27 08:46:24 +0700

    fiix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,9 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from 'src/typeorm/entities/Account';
 import { Role } from 'src/typeorm/entities/Roles';
-import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { RefreshToken } from 'src/typeorm/entities/RefreshToken';
-import { AuthService } from './service/auth/auth.service';
+
 
 
 
