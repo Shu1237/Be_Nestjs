921bcc3d4ce9cd8d11b68deca0091e71f026d85c
Shu1237 <ttei8191@gmail.com>
2025-06-02 10:10:02 +0700

    fix

--- src/employees/middleware/emloyees/emloyees.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EmloyeesMiddleware } from './emloyees.middleware';
-
-describe('EmloyeesMiddleware', () => {
-  it('should be defined', () => {
-    expect(new EmloyeesMiddleware()).toBeDefined();
-  });
-});
