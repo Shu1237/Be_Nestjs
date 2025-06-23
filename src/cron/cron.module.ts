ab327255b160263a0ccf8bab4191218891367655
trimikey <leductri12321@gmail.com>
2025-06-23 08:41:43 +0700

    feat: CJ moive, schedule

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,17 +1,29 @@
-import { Module } from "@nestjs/common";
-import { SeatAutoReleaseService } from "./seat/seat-auto-release.service";
-import { MyGateWayModule } from "src/gateways/seat.gateway.module";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ScheduleSeat } from "src/typeorm/entities/cinema/schedule_seat";
-import { RedisModule } from "src/redis/redis.module";
-import { RefreshToken } from "src/typeorm/entities/user/refresh-token";
-import { RefreshTokenService } from "./refresh-token/refresh-token.service";
-
+import { Module } from '@nestjs/common';
+import { SeatAutoReleaseService } from './seat/seat-auto-release.service';
+import { MyGateWayModule } from 'src/gateways/seat.gateway.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleSeat } from 'src/typeorm/entities/cinema/schedule_seat';
+import { RedisModule } from 'src/redis/redis.module';
+import { RefreshToken } from 'src/typeorm/entities/user/refresh-token';
+import { RefreshTokenService } from './refresh-token/refresh-token.service';
+import { MovieExpireCheckService } from './movie/movieExpireCheck.service';
+import { Movie } from 'src/typeorm/entities/cinema/movie';
+import { ScheduleExpireCheckService } from './schedule/ScheduleExpireCheck.service';
+import { Schedule } from 'src/typeorm/entities/cinema/schedule';
 
 @Module({
-    imports: [ RedisModule,MyGateWayModule,TypeOrmModule.forFeature([RefreshToken,ScheduleSeat])], 
-    providers: [SeatAutoReleaseService, RefreshTokenService],
-    exports: [SeatAutoReleaseService , RefreshTokenService],
-    controllers: [],
+  imports: [
+    RedisModule,
+    MyGateWayModule,
+    TypeOrmModule.forFeature([RefreshToken, ScheduleSeat, Movie, Schedule]),
+  ],
+  providers: [
+    SeatAutoReleaseService,
+    RefreshTokenService,
+    MovieExpireCheckService,
+    ScheduleExpireCheckService,
+  ],
+  exports: [SeatAutoReleaseService, RefreshTokenService],
+  controllers: [],
 })
-export class CronModule { }
+export class CronModule {}
