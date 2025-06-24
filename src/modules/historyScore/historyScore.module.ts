a877dd4b0340332530742e6c69f462cd9db54d34
Shu1237 <ttei8191@gmail.com>
2025-06-24 16:20:07 +0700

    update

---
+++ src/modules/historyScore/historyScore.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { HistoryScoreController } from "./historyScore.controller";
+import { HistoryScoreService } from "./historyScore.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { HistoryScore } from "src/database/entities/order/history_score";
+
+
+@Module({
+    imports: [TypeOrmModule.forFeature([HistoryScore])], // Add your entities here if needed
+    controllers: [HistoryScoreController],
+    providers: [HistoryScoreService],
+
+})
+export class HistoryScoreModule {}
