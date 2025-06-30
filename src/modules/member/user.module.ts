a718929bdc2bc9c801ffb43fdc9a73b0507127f3
Shu1237 <ttei8191@gmail.com>
2025-06-30 12:33:00 +0700

    update fix

--- src/modules/member/user.module.ts
+++ src/modules/member/user.module.ts
@@ -10,11 +10,11 @@ import { ProfileController } from './controllers/profile.controller';
 import { ProfileService } from './services/profile.service';
 import { BarcodeService } from 'src/common/barcode/barcode.service';
 import { BarcodeController } from 'src/common/barcode/barcode.controller';
-import { R2ConfigService } from 'src/common/config/r2.config';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Role])],
-  providers: [UserService, ProfileService, BarcodeService, R2ConfigService],
+  providers: [UserService, ProfileService, BarcodeService],
   controllers: [UserController, ProfileController, BarcodeController],
   exports: [UserService, ProfileService, BarcodeService],
 })
