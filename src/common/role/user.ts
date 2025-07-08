ffc3ebfeff0f1718146a613b895fe9a9009323f6
Shu1237 <ttei8191@gmail.com>
2025-07-08 10:06:17 +0700

    Update user.ts

--- src/common/role/user.ts
+++ src/common/role/user.ts
@@ -2,8 +2,8 @@ import { ForbiddenException } from "@nestjs/common";
 import { Role } from "../enums/roles.enum";
 import { JWTUserType } from "../utils/type";
 
-export const checkUserRole = (user: JWTUserType, msg: string, id: string): void => {
-    if (user.role_id === Role.USER && user.account_id !== id.toString()) {
+export const checkUserRole = (user: JWTUserType, msg: string,id:string): void => {
+    if (user.role_id === Role.USER && id.toString() !== user.account_id.toString()) {
         throw new ForbiddenException(msg);
     }
 }
