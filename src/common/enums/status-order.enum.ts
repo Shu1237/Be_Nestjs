27674eb8d5612d6b172c2511eaf785284bb8f6c0
Shu1237 <ttei8191@gmail.com>
2025-07-19 20:19:26 +0700

    update fix enum

--- src/common/enums/status-order.enum.ts
+++ src/common/enums/status-order.enum.ts
@@ -1,8 +1,7 @@
 export enum StatusOrder {
     SUCCESS = 'success',
     FAILED = 'failed',
-    PENDING = 'pending',
-    REFUND = 'refund',
+    PENDING = 'pending'
 }
 export const StatusOrderWithAll = ['all', ...Object.values(StatusOrder)] as const;
 export type StatusOrderWithAllType = typeof StatusOrderWithAll[number];
