1159239705abe9929f6b1f824c961f8dafdbbfe7
Shu1237 <ttei8191@gmail.com>
2025-08-01 09:27:46 +0700

    refactor: reorder import statements for clarity in refresh-token.guard.ts

--- src/common/guards/refresh-token.guard.ts
+++ src/common/guards/refresh-token.guard.ts
@@ -1,23 +1,19 @@
 
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-  ForbiddenException,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthService } from 'src/modules/auth/auth.service';
 import * as jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
 import { InternalServerErrorException } from '../exceptions/internal-server-error.exception';
+import { UnauthorizedException } from '../exceptions/unauthorized.exception';
+import { ForbiddenException } from '../exceptions/forbidden.exception';
 
 @Injectable()
 export class RefreshGuard implements CanActivate {
   constructor(
     private authService: AuthService,
     private readonly configService: ConfigService,
-  ) {}
+  ) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest<Request>();
